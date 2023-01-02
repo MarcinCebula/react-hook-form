51affc2fdda95688adbe3225bb89decf45ccc599
Marcin K Cebula <Marcin.K.Cebula@gmail.com>
2023-01-02 11:20:39 -0500

    force focus

--- src/logic/focusFieldBy.ts
+++ src/logic/focusFieldBy.ts
@@ -1,6 +1,7 @@
 import { FieldRefs, InternalFieldName } from '../types';
 import { get } from '../utils';
 import isObject from '../utils/isObject';
+import isFunction from '../utils/isFunction';
 
 const focusFieldBy = (
   fields: FieldRefs,
@@ -18,7 +19,7 @@ const focusFieldBy = (
           _f.ref.focus();
           break;
         } else if (_f.refs && _f.refs[0].focus) {
-          _f.refs[0].focus();
+           isFunction(_f.refs[0]?.scrollIntoView) && _f.refs[0].scrollIntoView();
           break;
         }
       } else if (isObject(currentField)) {
